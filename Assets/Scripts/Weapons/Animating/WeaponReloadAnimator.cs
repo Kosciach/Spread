9f041d37ff459b3780663e18c4bb4d584e609af4
Kosciach <97178996+Kosciach@users.noreply.github.com>
2023-05-29 22:13:32 +0200

    3.1.1 Reload animations prototype
    Created a super simple system for creating key like data for animations that are then used to move player hands.
    This is only experimental.

--- Assets/Scripts/Weapons/Animating/WeaponReloadAnimator.cs
+++ Assets/Scripts/Weapons/Animating/WeaponReloadAnimator.cs
@@ -6,40 +6,60 @@ public class WeaponReloadAnimator : MonoBehaviour
 {
     [Header("====References====")]
     [SerializeField] PlayerStateMachine _playerStateMachine;
+    [Space(10)]
+    [SerializeField] Transform _rightHandIk;
+    [SerializeField] Transform _rightHandIk_Hint;
+    [Space(5)]
+    [SerializeField] Transform _leftHandIk;
+    [SerializeField] Transform _leftHandIk_Hint;
 
 
     [Space(20)]
     [Header("====Debugs====")]
     [SerializeField] bool _isReloading; public bool IsReloading { get { return _isReloading; } }
 
-    private Transform _rightHandIk => _playerStateMachine.AnimatingControllers.Weapon.RightHandIk;
-    private Transform _leftHandIk => _playerStateMachine.AnimatingControllers.LeftHand.LeftHandIk;
 
 
 
 
 
-    public void Reload(ReloadAnim reloadAnim)
+    public void Reload(WeaponReloadAnimData reloadAnim)
     {
         if (!_playerStateMachine.MovementControllers.VerticalVelocity.GravityController.IsGrounded) return;
         if (_playerStateMachine.CombatControllers.Combat.EquipedWeaponController.Wall.IsWall) return;
 
-        _playerStateMachine.CombatControllers.Combat.EquipedWeaponController.Aim.ToggleAimBool(false);
-
         _isReloading = true;
+        _playerStateMachine.AnimatingControllers.IkLayers.SetLayerWeight(PlayerIkLayerController.LayerEnum.WeaponReload, true, 10);
 
-        reloadAnim.Play(_rightHandIk, _leftHandIk);
+        StartCoroutine(AnimRightHand(reloadAnim));
+    }
 
-        StartCoroutine(Siema());
+
+
+    public void ReloadFinish()
+    {
+        _isReloading = false;
+        _playerStateMachine.AnimatingControllers.IkLayers.SetLayerWeight(PlayerIkLayerController.LayerEnum.WeaponReload, false, 10);
     }
 
 
 
 
-    private IEnumerator Siema()
-    {
-        yield return new WaitForSeconds(2);
 
-        _isReloading = false;
+
+    private IEnumerator AnimRightHand(WeaponReloadAnimData reloadAnim)
+    {
+        for (int i = 0; i < reloadAnim.RightHandTransforms.Length; i++)
+        {
+            yield return new WaitForSeconds(reloadAnim.RightHandTransforms[i].TimeToHappen);
+
+
+            Debug.Log(i);
+            LeanTween.rotateLocal(_rightHandIk.gameObject, reloadAnim.RightHandTransforms[i].Rot, reloadAnim.RightHandTransforms[i].Rot_Time);
+
+            _rightHandIk.LeanMoveLocal(reloadAnim.RightHandTransforms[i].Pos, reloadAnim.RightHandTransforms[i].Pos_Time);
+            _rightHandIk_Hint.LeanMoveLocal(reloadAnim.RightHandTransforms[i].Hint_Pos, reloadAnim.RightHandTransforms[i].Pos_Time);
+        }
+        ReloadFinish();
     }
 }
