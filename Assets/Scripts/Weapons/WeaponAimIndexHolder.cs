726825a7d4e5c77776460aac4fce720c8f4559f4
Kosciach <97178996+Kosciach@users.noreply.github.com>
2023-10-29 11:07:56 +0100

    Refreshing
    I have moved project files to diffrent folder to slowly put those files back to the games, check for bugs and add fixes.

--- Assets/Scripts/Weapons/WeaponAimIndexHolder.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class WeaponAimIndexHolder : MonoBehaviour
-{
-    [SerializeField] int _weaponAimIndex; public int WeaponAimIndex { get { return _weaponAimIndex; } set { _weaponAimIndex = value; } }
-}
