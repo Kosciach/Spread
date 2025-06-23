0651cc867241823548c9319cb4b335e48c98a396
Kosciach <maciek.olszok.15@wp.pl>
2025-06-23 14:38:39 +0200

    Ladded interaction prompt positioning

--- Assets/_Features/Interactions/Interactable.cs
+++ Assets/_Features/Interactions/Interactable.cs
@@ -1,41 +1,54 @@
 using UnityEngine;
 using QuickOutline;
 using System;
+using SaintsField;
+using SaintsField.Playa;
 
 namespace Spread.Interactions
 {
     public abstract class Interactable : MonoBehaviour
     {
+        [LayoutStart("References", ELayout.TitleBox)]
         [SerializeField] private Outline _outline;
         [SerializeField] protected Transform _promptPosRef;
         public Transform PromptWorldRef => _promptPosRef;
-
-        [SerializeField] public InteractableData InteractableData;
+        [SerializeField, SaintsRow(inline: true)] public InteractableData InteractableData;
 
         private float _outlineWidth;
         public Action<Interaction> OnInteraction;
 
-
-        protected virtual void Awake()
+        
+        private void Awake()
         {
             _outlineWidth = _outline.OutlineWidth;
             _outline.OutlineWidth = 0;
+            
+            OnAwake();
         }
-
-        public virtual void Select(Transform p_player)
+        
+        public void Select(Transform p_player)
         {
             _outline.OutlineWidth = _outlineWidth;
+            
+            OnSelect(p_player);
         }
-
-        public virtual void UnSelect()
+        
+        public void UnSelect()
         {
             _outline.OutlineWidth = 0;
+            
+            OnUnSelect();
         }
+        
+        protected virtual void OnAwake() { }
+        protected virtual void OnSelect(Transform p_playe) { }
+        protected virtual void OnUnSelect() { }
     }
 
     [System.Serializable]
     public class InteractableData
     {
+        [LayoutStart("./Interactions", ELayout.TitleBox)]
         public string Name;
         public Interaction[] Interactions;
     }
--- Assets/_Features/Ladder/Ladder.cs
+++ Assets/_Features/Ladder/Ladder.cs
@@ -1,5 +1,8 @@
+using System;
 using UnityEngine;
 using System.Collections.Generic;
+using SaintsField;
+using SaintsField.Playa;
 
 namespace Spread.Ladder
 {
@@ -7,6 +10,45 @@ namespace Spread.Ladder
 
     public class Ladder : Interactable
     {
+        [LayoutStart("Settings", ELayout.TitleBox)]
+        [SerializeField] private Vector3 _topPromptPos;
+        [SerializeField] private Vector3 _bottomPromptPos;
+        
+        
+        protected override void OnSelect(Transform p_player)
+        {
+            SetPromptPos(p_player.position);
+        }
 
+        protected override void OnUnSelect()
+        {
+            
+        }
+
+        private void SetPromptPos(Vector3 p_playerPos)
+        {
+            _promptPosRef.localPosition = IsPlayerTop(p_playerPos)
+                ? _topPromptPos
+                : _bottomPromptPos;
+        }
+
+        private bool IsPlayerTop(Vector3 p_playerPos)
+        {
+            float playerDistanceToTopPrompt = Vector3.Distance(p_playerPos, _topPromptPos);
+            float playerDistanceToBottomPrompt = Vector3.Distance(p_playerPos, _bottomPromptPos);
+
+            return playerDistanceToTopPrompt < playerDistanceToBottomPrompt;
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawSphere(transform.TransformPoint(_topPromptPos), 0.1f);
+            
+            Gizmos.color = Color.gray;
+            Gizmos.DrawSphere(transform.TransformPoint(_bottomPromptPos), 0.1f);
+        }
+#endif
     }
 }
